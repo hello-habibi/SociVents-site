27b49eb21ee689152a104d6c1ec5c7a4eef404a6
Habibur Rahman <hr13338@gmail.com>
2024-03-03 02:07:48 +0600

    react router is setted up with the header of the site

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,33 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Root from './components/root/Root.jsx'
+import Home from './components/home/Home.jsx'
+import About from './components/about/About.jsx'
+import Signin from './components/signin/Signin.jsx'
+import Register from './components/register/Register.jsx'
 
 const router = new createBrowserRouter([
   {
     path:'/',
-    element:<Root />
+    element:<Root />,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      },
+      {
+        path:'about',
+        element:<About></About>
+      },
+      {
+        path:'signin',
+        element:<Signin></Signin>
+      },
+      {
+        path:'register',
+        element:<Register></Register>
+      },
+    ]
   }
 ])
 
